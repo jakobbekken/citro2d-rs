2c7b0d1514463b2032978a9f035ee6a3cbd851e6
Jakob Moen Bekken <jakobmb@uia.no>
2026-05-21 17:44:31 +0200

    Add const colors

--- citro2d/src/color.rs
+++ citro2d/src/color.rs
@@ -4,13 +4,13 @@ pub struct Color {
 }
 
 impl Color {
-    pub fn rgb(r: u8, g: u8, b: u8) -> Self {
+    pub const fn rgb(r: u8, g: u8, b: u8) -> Self {
         Self::rgba(r, g, b, 255)
     }
 
-    pub fn rgba(r: u8, g: u8, b: u8, a: u8) -> Self {
+    pub const fn rgba(r: u8, g: u8, b: u8, a: u8) -> Self {
         Self {
-            value: unsafe { citro2d_sys::C2D_Color32(r, g, b, a) },
+            value: (r as u32) | ((g as u32) << 8) | ((b as u32) << 16) | ((a as u32) << 24),
         }
     }
 }
